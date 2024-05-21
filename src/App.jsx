8e8723229472fd5115b04dff3b43bb968ee661ad
Rooper13 <rooper3455@gmail.com>
2024-05-21 22:53:14 +0700

    hapus import

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { GlobalStyle } from './utils/GlobalStyle'
 import Login from "./pages/Login"
 import Registration from "./pages/Registration"
-import KonsultasiOnline from "./pages/KonsultasiOnline";
-import Penyakit from "./pages/Penyakit"
-import Penyakit1 from "./pages/Penyakit1"
-import Penyakit2 from "./pages/Penyakit2"
-import Penyakit3 from "./pages/Penyakit3"
-import Penyakit4 from "./pages/Penyakit4"
-import Penyakit5 from "./pages/Penyakit5"
-import Penyakit6 from "./pages/Penyakit6"
 const App = () => {
 
   return (
